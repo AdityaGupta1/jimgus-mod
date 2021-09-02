7049503c86e294e69855c4c0ea6cd04e2518e7fd
AdityaGupta1 <adityag@gmail.com>
2021-09-02 15:07:12 -0400

    initial commit

---
+++ src/main/java/org/sdoaj/jimgus/Jimgus.java
@@ -0,0 +1,24 @@
+package org.sdoaj.jimgus;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("jimgus")
+public class Jimgus {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Jimgus() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        LOGGER.info("jimgus preinit");
+    }
+}
