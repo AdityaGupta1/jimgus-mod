85c8f130982a92f3fb0d0fc2dace33f0b759c3ce
AdityaGupta1 <adityag@gmail.com>
2021-09-15 15:01:37 -0400

    add random to L-system

--- src/main/java/org/sdoaj/jimgus/util/LSystem.java
+++ src/main/java/org/sdoaj/jimgus/util/LSystem.java
@@ -1,11 +1,13 @@
 package org.sdoaj.jimgus.util;
 
+import java.util.Random;
+
 public abstract class LSystem {
     protected abstract String getSeed();
 
-    protected abstract String produce(char c);
+    protected abstract String produce(char c, Random random);
 
-    public String run(int iterations) {
+    public String run(int iterations, Random random) {
         String string = getSeed();
 
         for (int i = 0; i < iterations; i++) {
@@ -13,12 +15,16 @@ public abstract class LSystem {
             StringBuilder newString = new StringBuilder();
 
             for (char c : chars) {
-                newString.append(produce(c));
+                newString.append(produce(c, random));
             }
 
             string = newString.toString();
         }
 
         return string;
+    }
+
+    public String run(int iterations) {
+        return run(iterations, null);
     }
 }
